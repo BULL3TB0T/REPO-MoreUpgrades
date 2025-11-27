6e2edbaba1cb1121c61682b5fd71859857409c2a
BULLETBOT <csgodenisz@gmail.com>
2025-11-27 13:06:53 +0100

    1.5.8

--- MoreUpgrades/Plugin.cs
+++ MoreUpgrades/Plugin.cs
@@ -22,7 +22,7 @@ namespace MoreUpgrades
     {
         private const string modGUID = "bulletbot.moreupgrades";
         private const string modName = "MoreUpgrades";
-        private const string modVer = "1.5.7";
+        private const string modVer = "1.5.8";
 
         internal static Plugin instance;
         internal ManualLogSource logger;
